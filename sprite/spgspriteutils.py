59d004ace5aef2fd73ef1c70b93f215127a0e708
m1chaelwilliams <michaelwilliamspersonal@gmail.com>
2023-09-05 01:50:55 -0500

    Fixing some docstring gammar

--- sprite/spgspriteutils.py
+++ sprite/spgspriteutils.py
@@ -5,10 +5,10 @@ from math import degrees, atan2, sqrt
 
 def point_at(point: Vector2, target_point: Vector2, angle_offset: float = 0):
     '''
-    Returns the angle for the initial point to point at the target point.
+    Returns the angle from the origin point to the target point.
 
     Argument List:
-     - point:        (Initial Point)
+     - point:        (origin point)
      - target_point: (To be pointed at)
      - angle_offset: (Optional, added on the resulting angle)
     '''
@@ -21,6 +21,8 @@ def point_at(point: Vector2, target_point: Vector2, angle_offset: float = 0):
 def rotate_around_center(surface: Surface, angle: float, center_point: tuple[int, int]) -> [Surface, Rect]:
     '''
     Rotates image by given angle. Returns new surface and rect.
+    
+    "center_point" is based on world coordinates.
 
     Notes:
     Since pygame's Y coordinate is inverted, the angle is inverted aswell. This was solved by
@@ -36,6 +38,9 @@ def rotate_around_center(surface: Surface, angle: float, center_point: tuple[int
     return [surf, rect]
 
 def get_position_from_origin(origin: Vector2, angle: float, distance_from_center: int) -> Vector2:
+    '''
+    Returns a coordinate n distance away from an origin point at a given angle.
+    '''
     offset = Vector2()
     offset.from_polar((distance_from_center, angle))
     
